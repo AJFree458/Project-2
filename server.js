df7779f6b039d4b1ae9adf1bd2632ce49ab97133
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-04-29 20:24:22 -0400

    Initial html routes and server config

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+var PORT = process.env.PORT || 8080;
+
+var db = require("./models");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+require("./routes/apiroutes.js")(app);
+require("./routes/html-routes.js")(app);
+
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+        console.log("App listening on PORT " + PORT)
+    })
+})
