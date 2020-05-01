051915b50da9ec8a2b3b05f3e74bd3c7eec1d905
Vannak <vannaksuos@gmail.com>
2020-04-30 23:56:28 -0400

    adding to public file for authentication process

---
+++ config/middleware/isAuthenticated.js
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+module.exports = function (req, res, next) {
+  // If the user is logged in, continue with the request to the restricted route
+  if (req.user) {
+    return next();
+  }
+
+  // If the user isn't logged in, redirect them to the login page
+
+  return res.redirect("../public/html/login.html");
+};
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 var express = require("express");
 
 var router = express.Router();
@@ -5,3 +6,33 @@ var router = express.Router();
 router.get("/", function (req, res) {
   res.render("index");
 });
+
+// Requiring path to so we can use relative routes to our HTML files
+var path = require("path");
+
+// Requiring our custom middleware for checking if a user is logged in
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
+module.exports = function (app) {
+  app.get("/", function (req, res) {
+    // If the user already has an account send them to the volunteer page
+    if (req.user) {
+      res.redirect("./volunteer");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+
+  app.get("/login", function (req, res) {
+    // If the user already has an account send them to the volunteer page
+    if (req.user) {
+      res.redirect("./volunteer");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("./volunteer", isAuthenticated, function (req, res) {
+    res.sendFile(path.join(__dirname, "../public./volunteer.html"));
+  });
+};
