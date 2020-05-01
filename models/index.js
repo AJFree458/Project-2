6d7016719b010f3ae93ae149fcab754db6507ae7
Ernest Correale <ecorreale@me.com>
2020-05-01 12:41:58 -0400

    EVC

--- models/index.js
+++ models/index.js
@@ -14,14 +14,13 @@ const db = {};
 
 // let sequelize;
 // if (config.use_env_variable) {
-const sequelize = new Sequelize(
+let sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     dialect: "mysql",
   }
-  // config
 );
 
 fs.readdirSync(__dirname)
@@ -32,7 +31,6 @@ fs.readdirSync(__dirname)
   })
   .forEach((file) => {
     const model = sequelize["import"](path.join(__dirname, file));
-
     db[model.name] = model;
   });
 
@@ -40,40 +38,38 @@ Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
-  const env = process.env.NODE_ENV || "development";
-  const config = require(__dirname + "/../config/config.json")[env];
 });
 
 // const db = {};
 
 // let sequelize;
 // if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+//     sequelize = new Sequelize(process.env[config.use_env_variable], config);
 // } else {
-//   sequelize = new Sequelize(
-//     config.database,
-//     config.username,
-//     config.password,
-//     config
-//   );
+//     sequelize = new Sequelize(
+//         config.database,
+//         config.username,
+//         config.password,
+//         config
+//     );
 // }
-// },
-//     fs.readdirSync(__dirname)
-//         .filter((file) => {
-//             return (
-//                 file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-//             );
-//         })
-//         .forEach((file) => {
-//             const model = sequelize["import"](path.join(__dirname, file));
-//             db[model.name] = model;
-//         });
 
-//     Object.keys(db).forEach((modelName) => {
-//         if (db[modelName].associate) {
-//             db[modelName].associate(db);
-//         }
+// fs.readdirSync(__dirname)
+//     .filter((file) => {
+//         return (
+//             file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+//         );
+//     })
+//     .forEach((file) => {
+//         const model = sequelize["import"](path.join(__dirname, file));
+//         db[model.name] = model;
 //     });
+
+// Object.keys(db).forEach((modelName) => {
+//     if (db[modelName].associate) {
+//         db[modelName].associate(db);
+//     }
+// });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
