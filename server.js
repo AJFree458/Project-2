e07290df30d199c7406b86102c51f681306a0e07
Ernest Correale <ecorreale@me.com>
2020-05-01 11:32:41 -0400

    EVC

--- server.js
+++ server.js
@@ -1,28 +1,41 @@
 /* eslint-disable indent */
-require('dotenv').config()
-var express = require('express')
+var express = require("express");
+var exphbs = require("express-handlebars");
+var session = require("express-session");
+var passport = require("./config/passport");
 
-var app = express()
-var PORT = process.env.PORT || 8080
+require("dotenv").config();
+var express = require("express");
+
+var app = express();
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var PORT = process.env.PORT || 8080;
 
 // Require models for sync
-var db = require('./models')
+var db = require("./models");
 
 // Data parsing
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Static directory
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-// Load routes files
-// =============================================================
-require('./routes/htmlRoutes.js')(app)
-require('./routes/apiRoutes.js')(app)
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-// run Sync to synchronize models then start express
-db.sequelize.sync({ force: true }).then(() => {
-    app.listen(PORT, () => {
-        console.log('App listening on PORT ' + PORT)
-    })
-})
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+db.sequelize.sync({ force: true }).then(function () {
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
+});
