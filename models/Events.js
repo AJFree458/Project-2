d6b11c14776d563592408279987bdbc662bd24ab
Ernest Correale <ecorreale@me.com>
2020-05-01 11:44:45 -0400

    EVC

--- models/Events.js
+++ models/Events.js
@@ -1,103 +1,101 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable indent */
 module.exports = (sequelize, DataTypes) => {
+  const Events = sequelize.define("Events", {
+    eventId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      field: "id",
+    },
 
-    const Events = sequelize.define(
-        'Events', {
-            eventId: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                primaryKey: true,
-                autoIncrement: true,
-                field: 'id'
-            },
+    eventTitle: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isAlphanumeric: true,
+      },
+    },
 
-            eventTitle: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                validate: {
-                    isAlphanumeric: true
-                }
-            },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isAlphanumeric: true,
+      },
+    },
 
-            address: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                validate: {
-                    isAlphanumeric: true
-                }
-            },
+    eventStartDate: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isAlphanumeric: true,
+      },
+    },
 
-            eventStartDate: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                validate: {
-                    isAlphanumeric: true
-                }
-            },
+    eventStartTime: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isAlphanumeric: true,
+      },
+    },
 
-            eventStartTime: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                validate: {
-                    isAlphanumeric: true
-                }
-            },
+    eventEndDate: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        isAlphanumeric: true,
+      },
+    },
 
-            eventEndDate: {
-                type: DataTypes.STRING,
-                allowNull: true,
-                validate: {
-                    isAlphanumeric: true
-                }
-            },
+    eventEndTime: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        isAlphanumeric: true,
+      },
+    },
 
-            eventEndTime: {
-                type: DataTypes.STRING,
-                allowNull: true,
-                validate: {
-                    isAlphanumeric: true
-                }
-            },
+    regLimit: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        isNumeric: true,
+      },
+    },
 
-            regLimit: {
-                type: DataTypes.INTEGER,
-                allowNull: true,
-                validate: {
-                    isNumeric: true
-                }
-            },
+    duration: {
+      type: DataTypes.DECIMAL(2),
+      allowNull: true,
+      validate: {
+        isNumeric: true,
+      },
+    },
 
-            duration: {
-                type: DataTypes.DECIMAL(2),
-                allowNull: true,
-                validate: {
-                    isNumeric: true
-                }
-            },
+    effort: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        isAlphanumeric: true,
+      },
+    },
 
-            effort: {
-                type: DataTypes.STRING,
-                allowNull: true,
-                validate: {
-                    isAlphanumeric: true
-                }
-            },
+    eventSummary: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      validate: {
+        isAlphanumeric: true,
+      },
+    },
 
-            eventSummary: {
-                type: DataTypes.TEXT,
-                allowNull: true,
-                validate: {
-                    isAlphanumeric: true
-                }
-            },
-
-            eventDetail: {
-                type: DataTypes.TEXT,
-                allowNull: true,
-                validate: {
-                    isAlphanumeric: true
-                }
-            }
-
-        })
-}
+    eventDetail: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      validate: {
+        isAlphanumeric: true,
+      },
+    },
+  });
+};
